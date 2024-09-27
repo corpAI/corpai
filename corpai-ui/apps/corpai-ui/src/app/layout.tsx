6734aa288fa9b1596b57931c1c27b5e5192ef2d3
ssamudrala14 <sriteja@corpai.io>
2024-09-27 21:34:47 +0530

    Revert "feat: Login page and backend code"
    This reverts commit e90671fc7d9475b00e3f616dcad24a9eab878d4c.

--- corpai-ui/apps/corpai-ui/src/app/layout.tsx
+++ corpai-ui/apps/corpai-ui/src/app/layout.tsx
@@ -12,12 +12,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-      <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body>{children}</body>
     </html>
   );
